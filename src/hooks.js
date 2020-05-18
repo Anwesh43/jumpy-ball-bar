a9c8cfca1de008acf1859d9c896bcfe56fd690d6
Anwesh43 <anweshthecool0@gmail.com>
2020-05-19 03:27:14 +0530

    adding useStyle hook

--- src/hooks.js
+++ src/hooks.js
@@ -32,7 +32,7 @@ export const useDimension = () => {
             setH(window.innerHeight)
             return () => {
                 window.onresize = () => {
-                    
+
                 }
             }
         }
@@ -40,5 +40,31 @@ export const useDimension = () => {
     return {
         w,
         h
+    }
+}
+
+export const useStyle = (w, h, scale) => {
+    const y = (h / 2) * Math.sin(scale * Math.PI)
+    const x = w / 2
+    const position = 'absolute'
+    const background = '#4a148c'
+    const barW = Math.min(w, h) / 8
+    const radius = Math.min(w, h) / 6
+    return {
+        barStyle() {
+            const left = `${x - barW / 2}px`
+            const top = '0px'
+            const width = `${barW}px`
+            const height = `${y}px`
+            return {position, left, top, width, height, background}
+        },
+        ballStyle() {
+            const left = `${w / 2 - radius}px`
+            const top = `${y + radius}px`
+            const width = `${2 * radius}px`
+            const height = `${2 * radius}px`
+            const borderRadius = `50%`
+            return {position, left, top, width, height, borderRadius, background}
+        }
     }
 }
